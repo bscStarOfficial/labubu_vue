74fcf8929c86777235a64f0ea56c75698faca515
jc <pandangoup@outlook.com>
2026-01-24 09:34:00 +0800

    router

---
+++ src/js/contracts/labubu.js
@@ -0,0 +1,45 @@
+import defaultAbi from '@/abis/labubu';
+import {getAddress} from "@/js/config";
+import {getContract, getSendPram} from "@/js/web3";
+import {postMessage} from "@/js/transaction";
+
+export async function getDefaultContract() {
+  let defaultAddress = await getAddress('labubu');
+  return await getContract(defaultAbi, defaultAddress)
+}
+
+export async function dailyBurnRate(id) {
+  let contract = await getDefaultContract();
+  if (window?.ethereum?.platform == 'btn') {
+    return await contract.dailyBurnRate(id);
+  } else {
+    return await contract?.methods?.dailyBurnRate(id).call(getSendPram());
+  }
+}
+
+export async function triggerDailyBurnAndMint() {
+  let labubu = await getDefaultContract();
+  if (window?.ethereum?.platform == 'btn') {
+    await postMessage({
+      name: 'sendTx',
+      target: labubu.target,
+      data: labubu.interface.encodeFunctionData("triggerDailyBurnAndMint")
+    })
+  } else {
+    await labubu?.methods?.triggerDailyBurnAndMint().send(getSendPram());
+  }
+}
+
+export async function setDailBurnRate(rates) {
+  let labubu = await getDefaultContract();
+  if (window?.ethereum?.platform == 'btn') {
+    await postMessage({
+      name: 'sendTx',
+      target: labubu.target,
+      data: labubu.interface.encodeFunctionData("setDailBurnRate", rates)
+    })
+  } else {
+    await labubu?.methods?.setDailBurnRate(rates).send(getSendPram());
+  }
+}
+
---
+++ src/views/admin/manage.vue
@@ -0,0 +1,92 @@
+<script setup>
+import {onMounted, reactive, ref} from 'vue'
+import {dailyBurnRate, setDailBurnRate, triggerDailyBurnAndMint} from "@/js/contracts/labubu";
+
+const tab = ref(0)
+const loading = reactive([false]);
+const dailyBurnRates = ref([0, 0, 0])
+const dailyBurnRatesSet = ref(['', '', ''])
+
+onMounted(async () => {
+  await init()
+})
+
+async function init() {
+  console.log(await dailyBurnRate(0));
+  return;
+
+  let res = await Promise.all([
+    dailyBurnRate(0),
+    // dailyBurnRate(1),
+    // dailyBurnRate(2),
+  ]);
+  dailyBurnRates.value = [res[0], res[1], res[2]];
+  console.log(dailyBurnRates.value)
+}
+
+// async function doTriggerDailyBurnAndMint() {
+//   if (loading[0]) return;
+//   loading[0] = true;
+//   try {
+//     await triggerDailyBurnAndMint();
+//   } catch (e) {
+//     console.log(e)
+//   }
+//   loading[0] = false;
+// }
+
+async function doSetDailBurnRate() {
+  if (loading[0]) return;
+  loading[0] = true;
+  try {
+    await setDailBurnRate(dailyBurnRatesSet.value);
+  } catch (e) {
+    console.log(e)
+  }
+  loading[0] = false;
+}
+
+
+</script>
+<template>
+  <div class="liquidity">
+    <van-nav-bar title="LABUBU管理后台" class="l-navbar"/>
+    <div class="l-btns">
+      <div class="tabs">
+        <div :class="tab == 0 ? 'tab-a' : 'tab'">每日销毁</div>
+      </div>
+    </div>
+    <template v-if="tab == 0">
+      <div class="l-info mb10">
+        <div class="left">销毁比例</div>
+        <div class="right">{{ dailyBurnRates[0] }}</div>
+      </div>
+      <div class="l-info mb10">
+        <div class="left">项目方比例</div>
+        <div class="right">{{ dailyBurnRates[1] }}</div>
+      </div>
+      <div class="l-info mb10">
+        <div class="left">分红比例</div>
+        <div class="right">{{ dailyBurnRates[2] }}</div>
+      </div>
+
+      <div class="l-input mb16" style="margin-top: 20px">
+        <van-field v-model="dailyBurnRatesSet[0]" placeholder="输入销毁比例：1%输入1"/>
+      </div>
+      <div class="l-input mb16" style="margin-top: 20px">
+        <van-field v-model="dailyBurnRatesSet[1]" placeholder="输入项目方比例"/>
+      </div>
+      <div class="l-input mb16" style="margin-top: 20px">
+        <van-field v-model="dailyBurnRatesSet[2]" placeholder="输入分红比例"/>
+      </div>
+      <div v-if="loading[0]" class="l-btn">
+        <van-loading size="22" type="circular" color="#FFF"/>
+      </div>
+      <div v-else class="l-btn" @click="doSetDailBurnRate()">设置比例</div>
+      <div style="height: 20px;"></div>
+    </template>
+  </div>
+</template>
+<style scoped>
+@import "@/views/admin/manage.scss";
+</style>
