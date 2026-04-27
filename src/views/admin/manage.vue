ed05050d12e2aa6b041cc5c187474d20fd7cc4f5
etsc-new <pandangoup@outlook.com>
2026-04-28 01:20:31 +0800

    css

--- src/views/admin/manage.vue
+++ src/views/admin/manage.vue
@@ -5,6 +5,7 @@ import role from "./componets/role.vue";
 import maxAmount from "./componets/maxAmount.vue";
 import nft from "./componets/nft.vue";
 import feeDividend from "./componets/feeDividend.vue";
+import sellFee from "./componets/sellFee.vue";
 
 const tab = ref(0)
 onMounted(async () => {
@@ -21,6 +22,7 @@ onMounted(async () => {
         <div :class="tab == 2 ? 'tab-a' : 'tab'" @click="tab = 2">入单金额</div>
         <div :class="tab == 3 ? 'tab-a' : 'tab'" @click="tab = 3">NFT</div>
         <div :class="tab == 4 ? 'tab-a' : 'tab'" @click="tab = 4">手续费分红</div>
+        <div :class="tab == 5 ? 'tab-a' : 'tab'" @click="tab = 5">卖出手续费</div>
       </div>
     </div>
     <dailyBurn v-if="tab == 0"></dailyBurn>
@@ -28,6 +30,7 @@ onMounted(async () => {
     <maxAmount v-if="tab == 2"></maxAmount>
     <nft v-if="tab == 3"></nft>
     <feeDividend v-if="tab == 4"></feeDividend>
+    <sellFee v-if="tab == 5"></sellFee>
   </div>
 </template>
 <style scoped>
